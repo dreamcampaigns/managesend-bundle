1b2f02d187d30696d3b5d5bf231d93ef627cc2d4
David Koshkarman <davidk@vdsys.com>
2020-01-01 12:09:40 -0500

    init

---
+++ ManagesendApiBundle.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This file is part of the Managesend Bundle package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Managesend\ApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class ManagesendApiBundle
+ * @package Managesend\ApiBundle
+ */
+class ManagesendApiBundle extends Bundle
+{
+}
